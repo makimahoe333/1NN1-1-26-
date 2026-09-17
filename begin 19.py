433990547212ff6194bd2b5add842e5e55a949c8
makimahoe333 <mobilefifer8@gmail.com>
2026-09-17 18:26:46 +0300

    Add files via upload

---
+++ begin 19.py
@@ -0,0 +1,10 @@
+x1 = float(input("Введите x1: "))
+y1 = float(input("Введите y1: "))
+x2 = float(input("Введите x2: "))
+y2 = float(input("Введите y2: "))
+a = abs(x1 - x2)
+b = abs(y1 - y2)
+p = 2 * (a + b)
+s = a * b
+print("Периметр прямоугольника:", p)
+print("Площадь прямоугольника:", s)
---
+++ begin18.py
@@ -0,0 +1,7 @@
+a = float(input("Введите A: "))
+b = float(input("Введите B: "))
+c = float(input("Введите C: "))
+ac = abs(a - c)
+bc = abs(b - c)
+p = ac * bc
+print("Произведение длин отрезков:", p)
